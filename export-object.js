abee591197edbfca769f0a1478a9e86813a497a9
Prem Bikram Limbu <prem.bikram.limbu@gmail.com>
2018-07-29 17:48:57 +0530

    Add files via upload

---
+++ export-object.js
@@ -0,0 +1,6 @@
+//file: export-object.js
+module.exports = {
+  name:'Node JS',
+  surname: 'Mr. C',
+  getFullname: function(){return `${this.name} - ${this.surname}`}
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+//file: index.js
+var user = require('./export-object');
+console.log(user.name);
+console.log(user.surname);
+console.log(user.getFullname());
